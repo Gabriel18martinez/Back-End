663da73057172a5bf4971c465be631438603f799
Gabriel18martinez <77814885+Gabriel18martinez@users.noreply.github.com>
2023-05-17 11:25:23 -0300

    Add files via upload

---
+++ CriaConexao.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CriaConexao{
+
+    public Connection recuperarConexao() throws SQLException{
+        Connection connection = DriverManager.getConnection
+        ("jdbc:mysql://localhost/dbteste?useTimezone=true&serverTimezone=UTC", "root", "mysqlroot");
+        return connection;
+    }        
+
+}
